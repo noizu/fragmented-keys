4bd1c7eaca4cbd093b6454de0002383f077160b0
keith <keith.brings@noizu.com>
2014-01-24 22:49:23 +0000

    Updating directory layout, switching to composer autoload

--- test/FragmentedKeysTest.php
+++ test/FragmentedKeysTest.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . "/../autoload.php");
+require_once(__DIR__ . "/../vendor/autoload.php");
 use \NoizuLabs\FragmentedKeys;
 use \NoizuLabs\FragmentedKeys\Tag;
 use \NoizuLabs\FragmentedKeys\Key;
